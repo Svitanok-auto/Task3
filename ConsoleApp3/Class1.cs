b2952491787bac8b5a6287422cf7c47dfa924d85
Sveta <bogatovasvetlanau@gmail.com>
2021-02-05 16:41:24 +0200

    Initial commit

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+
+    public class Class1 : ArrayBorders
+    {
+        public int Border { get; set; }
+
+        public Class1()
+        {
+            Console.WriteLine("What is the Value, should be more than 0");
+            int i = 0;
+
+            while (i < 10)
+            {
+
+                string InputString = Console.ReadLine();
+                if (InputString.ToString() != string.Empty) // the Convert fails when ""
+                {
+                    try
+                    {
+                        Border = Convert.ToInt32(InputString);
+                        Console.WriteLine("Border is " + Border);
+
+                        break;
+                    }
+                    catch (SystemException sex)
+                    {
+                        // this class's error string
+                        string LastError = sex.Message;
+                        Console.WriteLine("Incorrect Input, error" + LastError);
+                        Console.WriteLine("Try again to populate correct border");
+                        i++;
+
+                    }
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Empty input");
+                }
+            }
+        }
+             public override int CheckLowerUpper()
+        {
+            int result = Border;
+            return result;
+        }
+
+      
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public class ArrayBorders
+    {
+        public virtual int CheckLowerUpper()
+        {
+            return 0;
+
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set the lower value of the diapason!");
+            ArrayBorders border1 = new Class1();
+            Console.WriteLine("Lower Border : " + border1.CheckLowerUpper());
+            int Lower = border1.CheckLowerUpper();
+
+            Console.WriteLine("Set the upper value of the diapason!");
+            ArrayBorders border2 = new Class1();
+            Console.WriteLine("Upper Border : " + border2.CheckLowerUpper());
+            int Upper = border2.CheckLowerUpper();
+
+            int Number = 0;
+            Number = (Upper - Lower + 1);
+
+            Console.WriteLine("Count of values : " + Number);
+
+            int sum = 0;
+            if (Number > 10)
+
+            {
+                int[] NumberArray = new int[Number];
+                int i = 0;
+                do
+                {
+                    NumberArray[i] = Lower + i;
+                    Console.WriteLine("Array: " + NumberArray[i]);
+                    i++;
+                    Console.WriteLine("i: " + i);
+                }
+
+                while ((Lower + i <= Upper) && (i <= Number));
+
+                for (int u = 0; u < Number; u++)
+                {
+                    if ((NumberArray[u] % 3 == 0) && (NumberArray[u] % 5 != 0)) // It is divisible by 3 and not divisible by 5
+                    {
+                        sum = sum + NumberArray[u];
+                    }
+                }
+
+                Console.WriteLine("Result Sum : " + sum);
+
+            }
+            else
+            {
+                Console.WriteLine("Less then 10 values diapason, exit");
+            }
+            }
+
+    }
+}
